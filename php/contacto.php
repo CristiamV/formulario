930c72110dd520fdcfc8f9e10fee9c1db417b46d
cristiam14 <35568507+cristiam14@users.noreply.github.com>
2018-06-27 23:09:28 -0400

    Add files via upload

--- php/contacto.php
+++ php/contacto.php
@@ -1,71 +1,46 @@
 <?php
-/*Formulario de contacto HTML5, PHP Y Bootstraps
-Creado por: www.render2web.com
-Version: 1.1*/
 
-//Comprobamos que se haya presionado el boton enviar
 if(isset($_POST['enviar'])){
-	//Guardamos en variables los datos enviados
-	$nombre = $_POST['nombre'];
-	$email = $_POST['email'];
-	$telefono = $_POST['telefono'];
-	$ciudad = $_POST['ciudad'];
-	$pais = $_POST['pais'];
-	$mensaje = $_POST['mensaje'];
-	
-	///Validamos del lado del servidor que el nombre y el email no estén vacios
-	if($nombre == ''){
-		echo "Debe ingresar su nombre";
-	}
-	else if($email == ''){
-		echo "Debe ingresar su email";
-}else{
-	$para = "11-11055@usb.ve";//Email al que se enviará
-	$asunto = "Contacto para su sitio web";//Puedes cambiar el asunto del mensaje desde aqui
-	//Este sería el cuerpo del mensaje
-	$mensaje = "
-		<table border='0' cellspacing='3' cellpadding='2'>
-		  <tr>
-			<td width='30%' align='left' bgcolor='#f0efef'><strong>Nombre:</strong></td>
-			<td width='80%' align='left'>$nombre</td>
-		  </tr>
-		  <tr>
-			<td align='left' bgcolor='#f0efef'><strong>E-mail:</strong></td>
-			<td align='left'>$email</td>
-		  </tr>
-		   <tr>
-			<td width='30%' align='left' bgcolor='#f0efef'><strong>Teléfono:</strong></td>
-			<td width='70%' align='left'>$telefono</td>
-		  </tr>
-		  <tr>
-			<td width='30%' align='left' bgcolor='#f0efef'><strong>Ciudad:</strong></td>
-			<td width='70%' align='left'>$ciudad</td>
-		  </tr>
-		  <tr>
-			<td width='30%' align='left' bgcolor='#f0efef'><strong>País:</strong></td>
-			<td width='70%' align='left'>$pais</td>
-		  </tr>
-		  <tr>
-			<td align='left' bgcolor='#f0efef'><strong>Comentario:</strong></td>
-			<td align='left'>$mensaje</td>
-		  </tr>
-	</table>	
-";	
-	
-//Cabeceras del correo
-    $headers = "From: $nombre <$email>\r\n"; //Quien envia?
-    $headers .= "X-Mailer: PHP5\n";
-    $headers .= 'MIME-Version: 1.0' . "\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; //
-	
-//Comprobamos que los datos enviados a la función MAIL de PHP estén bien y si es correcto enviamos
-	if(mail($para, $asunto, $mensaje, $headers)){
-		echo "Su mensaje se ha enviado correctamente";
-		echo "<br />";
-		echo '<a href="../index.html">Volver</a>';
-	}else{
-		echo "Hubo un error en el envío inténtelo más tarde";
-	}
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'lontian14@gmail.com';                 // SMTP username
+    $mail->Password = 'c21175861';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('lontian14@gmail.com', 'pollo');
+    $mail->addAddress('cristiamvasquez@gmail.com', 'cris');     // Add a recipient
+    //$mail->addAddress('ellen@example.com');               // Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //cristiamvasquez@gmail.com$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'prueba';
+    $mail->Body    = 'prueeebbbaaaa <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
 }
-}	
-?>
+}
